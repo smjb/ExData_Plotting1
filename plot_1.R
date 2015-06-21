0918dabd3dba25553348f6476ac848d25e1488c3
smjb <smjb@users.noreply.github.com>
2015-06-21 18:34:44 +0800

    added comments

--- plot_1.R
+++ plot_1.R
@@ -19,6 +19,7 @@ power_data$Sub_metering_2[which(power_data$Sub_metering_2=="?")] <- NA
 power_data$Sub_metering_3[which(power_data$Sub_metering_3=="?")] <- NA
 power_data$Sub_metering_3[which(power_data$Sub_metering_3=="")] <- NA
 
+# reclass column
 power_data$Global_active_power <- as.numeric(power_data$Global_active_power) 
 power_data$Global_reactive_power <- as.numeric(power_data$Global_reactive_power)
 power_data$Global_intensity <- as.numeric(power_data$Global_intensity)
@@ -27,10 +28,12 @@ power_data$Sub_metering_1 <- as.numeric(power_data$Sub_metering_1)
 power_data$Sub_metering_2 <- as.numeric(power_data$Sub_metering_2)
 power_data$Sub_metering_3 <- as.numeric(power_data$Sub_metering_3)
 
+#create proper Date, Time and DateTime object
 p2 <- transform(power_data, d = dmy(Date), t = hms(Time))
 power_data <- transform(p2, dt = d+t)
 rm(p2)
 
+# plot as PNG
 png("plot_1.png", width = 480, height = 480, units = "px", bg = "transparent")
 
 hist(power_data$Global_active_power, freq=T, col="red", xlab="Global Active Power (kilowatts)", ylim = range(0,1200), main = "Global Active Power")
